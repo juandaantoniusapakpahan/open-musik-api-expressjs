3d252d2e07a5d47679ff4e74d5c835a12862ca37
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-02-17 14:48:30 +0700

    dislike

--- src/controllers/SongLike/SongLikeController.js
+++ src/controllers/SongLike/SongLikeController.js
@@ -63,3 +63,62 @@ exports.likeSong = BigPromise(async (req, res, next) => {
     next(error);
   }
 });
+
+exports.dislikeSong = BigPromise(async (req, res, next) => {
+  try {
+    const { songId } = req.params;
+
+    const queryCheckSong = {
+      text: "SELECT * FROM songs WHERE id = $1",
+      values: [songId],
+    };
+
+    const resultCheckSong = await _pool.query(queryCheckSong);
+
+    if (resultCheckSong.rows.length < 1) {
+      throw new NotFoundError("No song found");
+    }
+
+    // User id
+    const userId = req.user.id;
+    const QueryCheckLike = {
+      text: "SELECT * FROM song_ld where song_id = $1 AND user_id = $2",
+      values: [songId, userId],
+    };
+    const resultCheckLike = await _pool.query(QueryCheckLike);
+
+    let queryLike = "";
+    let arryLike = [];
+
+    if (resultCheckLike.rows.length < 1) {
+      const id = nanoid(16);
+      const createdAt = new Date().toISOString();
+      queryLike = "INSERT INTO song_ld VALUES($1, $2, $3, $4, $5)";
+      const array = [id, songId, userId, "dislike", createdAt];
+      arryLike.push(...array);
+    }
+
+    if (resultCheckLike.rows.length > 0) {
+      queryLike = "DELETE FROM song_ld WHERE song_id = $1 AND user_id = $2";
+      arryLike.push(songId, userId);
+      if (resultCheckLike.rows[0].status === "like") {
+        queryLike =
+          "UPDATE song_ld SET status = $3 WHERE song_id = $1 AND user_id = $2";
+        arryLike.push("dislike");
+      }
+    }
+    const query = {
+      text: queryLike,
+      values: arryLike,
+    };
+    await _pool.query(query);
+
+    res.status(200).json({
+      status: "success",
+    });
+
+    //
+  } catch (error) {
+    next(error);
+  }
+});
--- src/routes/songlikedislike.js
+++ src/routes/songlikedislike.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { isLoggin, customRoles } = require("../middlewares/user");
-const { likeSong } = require("../controllers/SongLike/SongLikeController");
+const {
+  likeSong,
+  dislikeSong,
+} = require("../controllers/SongLike/SongLikeController");
 
 router.route("/song/:songId/like").post(isLoggin, likeSong);
+router.route("/song/:songId/dislike").post(isLoggin, dislikeSong);
 
 module.exports = router;
